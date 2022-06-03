379df8e8723c2314e332d44377240ac2d71e1157
Rafael Innecco <rafael.a.innecco@usp.br>
2022-06-03 11:27:13 -0300

    Adiciona frases em lerolero.py

--- lerolero.py
+++ lerolero.py
@@ -2,8 +2,20 @@
 
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+        "Frase 1.1",
+        "Frase 1.2",
+        "Frase 1.3"
+        ]
+parte2 = [
+        "Frase 2.1",
+        "Frase 2.2",
+        "Frase 2.3"
+        ]
+parte3 = [
+        "Frase 3.1",
+        "Frase 3.2",
+        "Frase 3.3"
+        ]
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3)
