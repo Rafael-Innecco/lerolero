6c530be0f0101e5c5cce7f0c6f5c3b7a4cb99721
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-06-03 11:30:31 -0300

    remove else desnecessário

--- lerolero.py
+++ lerolero.py
@@ -24,9 +24,5 @@ if lingua == 2:
     parte1 = []
     parte2 = []
     parte3 = []
-else:
-    parte1 = []
-    parte2 = []
-    parte3 = []
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3)
