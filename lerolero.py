21c930ab0847e65ed02ce1d060888efa0c332d7c
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-06-03 11:26:09 -0300

    adds english version

--- lerolero.py
+++ lerolero.py
@@ -6,4 +6,15 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = input("Escolha alíngua; 1 - português; 2 - Inglês")
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+else:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3)
